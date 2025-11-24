292f40d92f6e47bb69c6de7ec4db3877a680f204
Dkay <kaludavid411@gmail.com>
2025-11-24 19:03:57 +0100

    Hero responsiveness fix

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -5,25 +5,25 @@ import { motion } from "framer-motion";
 
 export default function HeroSection() {
     return (
-        <section className="h-screen flex flex-row items-center justify-between px-20">
+        <section className="min-h-screen flex flex-col lg:flex-row items-center justify-between px-5  lg:px-20 py-10 pt-28 lg:py-0 gap-8 lg:gap-0">
             <motion.div
                 initial={{ opacity: 0, x: -100 }}
                 animate={{ opacity: 1, x: 0 }}
                 transition={{ duration: 0.8, ease: "easeOut" }}
             >
-                <h1 className="text-7xl font-bold text-start mb-8 text-black max-w-2xl">
+                <h1 className="text-5xl md:text-6xl lg:text-7xl font-bold text-start mb-8 text-black max-w-3xl">
                     Empowering Kids Through <span className="bg-text-gradient bg-clip-text text-transparent">Technology.</span>
                 </h1>
                 <p className="text-gray-500 font-medium mb-8 max-w-lg">
                     Join our vibrant community where children learn coding, robotics, and digital creativity in a fun, supportive environment.
                 </p>
-                <div className="flex space-x-4">
+                <div className="flex flex-col space-y-4 md:flex-row md:space-y-0 md:space-x-4">
                     <ScrollLink
                         to="programs"
                         smooth={true}
                         duration={600}
                         offset={-80}
-                        className="bg-gradient-purple-blue text-white font-semibold px-6 py-3 rounded-md cursor-pointer hover:bg-gradient-purple-blue/50 transition"
+                        className="bg-gradient-purple-blue text-white font-semibold px-6 py-3 rounded-md cursor-pointer hover:bg-gradient-purple-blue/50 transition text-center"
                     >
                         Get Started Today
                     </ScrollLink>
@@ -32,7 +32,7 @@ export default function HeroSection() {
                         smooth={true}
                         duration={600}
                         offset={-80}
-                        className="bg-transparent text-black font-semibold px-6 py-3 rounded-md cursor-pointer hover:bg-orange-400 border border-gray-400 transition"
+                        className="bg-transparent text-black font-semibold px-6 py-3 rounded-md cursor-pointer shadow-md hover:bg-gradient-purple-blue border border-gray-400 transition duration-1000 text-center"
                     >
                         Learn More
                     </ScrollLink>
@@ -42,13 +42,14 @@ export default function HeroSection() {
                 initial={{ opacity: 0, x: 100 }}
                 animate={{ opacity: 1, x: 0 }}
                 transition={{ duration: 0.8, ease: "easeOut" }}
+                className="w-full lg:w-auto"
             >
                 <Image 
                     src={"/hero-image.jpg"}
                     alt="Hero Image"
                     width={600}
                     height={400}
-                    className="rounded-xl"
+                    className="rounded-xl w-full h-auto"
                 />
             </motion.div>
         </section>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,7 +5,7 @@ import { Link as ScrollLink } from "react-scroll";
 
 export default function Navbar() {
   return (
-    <nav className="fixed top-0 left-0 w-full flex items-center justify-between px-8 md:px-20 border-b border-gray-300 bg-custom-lavender-80 text-black z-50">
+    <nav className="fixed top-0 left-0 w-full h-24 flex items-center justify-between px-5 md:px-20 border-b border-gray-300 bg-custom-lavender-80 text-black z-50">
         <Image
             src="/CodwizLogo.png"
             alt="Codwiz Logo"
@@ -14,7 +14,7 @@ export default function Navbar() {
         />
 
       <ul className="hidden md:flex space-x-8">
-        <li className="text-black font-semibold cursor-pointer hover:text-[#7443ee] transition">
+        <li className="text-black font-semibold cursor-pointer hover:text-[#7443ee] transition duration-300">
           <ScrollLink
             to="howitworks"
             smooth={true}
@@ -24,7 +24,7 @@ export default function Navbar() {
             Home
           </ScrollLink>
         </li>
-        <li className="text-black font-semibold cursor-pointer hover:text-[#7443ee] transition">
+        <li className="text-black font-semibold cursor-pointer hover:text-[#7443ee] transition duration-300">
           <ScrollLink
             to="pricing"
             smooth={true}
@@ -34,7 +34,7 @@ export default function Navbar() {
             About Us
           </ScrollLink>
         </li>
-        <li className="text-black font-semibold cursor-pointer hover:text-[#7443ee] transition">
+        <li className="text-black font-semibold cursor-pointer hover:text-[#7443ee] transition duration-300">
           <ScrollLink
             to="pricing"
             smooth={true}
