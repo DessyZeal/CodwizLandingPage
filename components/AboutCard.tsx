30d58d4b1e155a5140b6cdc170c860c85a1c7b31
Dkay <kaludavid411@gmail.com>
2025-11-29 19:44:50 +0100

    new push

--- components/AboutCard.tsx
+++ components/AboutCard.tsx
@@ -10,7 +10,7 @@ export default function AboutCard({icon: Icon, title, description}: AboutCardPro
     return (
         <div className="bg-white p-6 rounded-lg shadow-md text-center hover:shadow-xl transition-shadow duration-300">
             <div className="bg-gradient-purple-blue p-4 w-16 h-16 rounded-md flex items-center justify-center mb-4">
-                <Icon className=" text-white mb-4" size={32}/>
+                <Icon className=" text-white " size={32}/>
             </div>
             <h3 className="text-2xl font-bold text-black mb-3 text-start">{title}</h3>
             <p className="text-gray-500 text-start">{description}</p>
